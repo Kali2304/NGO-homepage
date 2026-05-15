0382e4d2039e1168879dff4b554be8b6d069fdf2
Kali2304 <156293389+Kali2304@users.noreply.github.com>
2026-05-15 22:14:30 +0530

    Initial NGO website homepage

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("NGO Awareness Website Loaded");
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+function changeSlide(){
+
+  slides[currentSlide].classList.remove("active");
+
+  currentSlide++;
+
+  if(currentSlide >= slides.length){
+    currentSlide = 0;
+  }
+
+  slides[currentSlide].classList.add("active");
+}
+
+setInterval(changeSlide, 3500);
